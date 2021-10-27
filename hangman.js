96c4cbe12ae23c5b4ce3c7c16522356d131dd15f
Rintarou <merlin.barzilai@gmail.com>
2021-10-27 11:06:40 +0200

    proper reset

--- hangman.js
+++ hangman.js
@@ -28,7 +28,7 @@ function onLoad() {
     let pad = document.getElementById('input-pad');
     Array.from(chars).forEach( ( char, id )=>{
         pad.insertAdjacentHTML('beforeend', `
-            <button class="btn-dark" id='`+char+`' class='btn btn-secondary'>`+char+`</button>
+            <button class="btn btn-dark" id='`+char+`'>`+char+`</button>
         `);
         document.getElementById( char ).addEventListener( 'click', characterClick );
     });
@@ -98,6 +98,8 @@ function rejouer() {
         obj.className='btn btn-secondary';
         obj.disabled = false;
     });
+
+    nbError = 0;
 
     word = 'grue';
 
